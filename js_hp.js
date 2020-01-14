8457e0716ab19082d90cca5e2879a911394cfd15
zhangyue8 <55032793+zhangyue8@users.noreply.github.com>
2020-01-14 12:59:12 -0500

    .

--- js_hp.js
+++ js_hp.js
@@ -4,6 +4,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-70px";
+    document.getElementById("navbar").style.top = "-30px";
   }
 }
