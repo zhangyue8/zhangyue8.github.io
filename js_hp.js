0725e34fa016dcfbf35b56f24742903b814241e1
zhangyue8 <55032793+zhangyue8@users.noreply.github.com>
2020-01-14 13:00:05 -0500

    .

--- js_hp.js
+++ js_hp.js
@@ -1,9 +1,9 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 60) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-30px";
+    document.getElementById("navbar").style.top = "-60px";
   }
 }
