ab1f2924dc6332279fad01a2a41678e6756929ac
zhangyue8 <55032793+zhangyue8@users.noreply.github.com>
2020-01-14 19:09:41 -0500

    。

--- homework/01-09/js.js
+++ homework/01-09/js.js
@@ -2,27 +2,18 @@ window.onload=function()
 {
 	var bt1=document.getElementById("pre");
 	var bt2=document.getElementById("next");
-	//获取img标签中的第一个元素结点
 	var img=document.getElementsByTagName("img")[0];
-	//创建一个数组用来保存图片的路径（即src属性）
 	var imgarr=["01-09_resource/marble-1.jpg","01-09_resource/marble-2.jpg","01-09_resource/marble-3.jpg","01-09_resource/marble-4.jpg","01-09_resource/marble-5.jpg"];
-	//设置数组的索引下标
 	 var index=0;
-	 //获取p标签
 	var pbq=document.getElementById("pid");
-	//设置提醒文字
 	pbq.innerHTML="All have "+imgarr.length+" pictures, now is NO."+(index+1);
-	//绑定事件
 	bt1.onclick=function()
 	{
-		//书写事件驱动程序
-		index--;//切换到上一张索引自减
-		//判断索引是否小于0
+		index--;
 		if(index<0)
 		{
-			index=imgarr.length-1;//目的是可以实现循环切换
+			index=imgarr.length-1;
 		}
-		//修改img的src属性
 		img.src=imgarr[index];
 		pbq.innerHTML="All have "+imgarr.length+" pictures, now is NO."+(index+1);
 	};
@@ -32,11 +23,9 @@ window.onload=function()
 		index++;
 		if(index>imgarr.length-1)
 		{
-			index=0;//目的是可以实现循环切换
+			index=0;
 		}
-		//修改img的src属性
 		img.src=imgarr[index];
-		//当点击按钮以后，重新设置p标签中的显示内容
 		pbq.innerHTML="All have "+imgarr.length+" pictures, now is NO."+(index+1);
 	};
 };
