ce78c6482091c31270c531d04eee4f7774d8986d
zhangyue8 <55032793+zhangyue8@users.noreply.github.com>
2020-01-23 13:22:43 -0500

    .

--- homework/01-16/01-16.js
+++ homework/01-16/01-16.js
@@ -1,13 +1,56 @@
+
+
 var realtimeURL="https://whiteboard.datawheel.us/api/google-analytics/realtime/111999474";
     var frequency = 1*1000;
 
-            function fetchData(){
+
+
+
+
+
+
+var width = 960,
+    height = 500;
+
+var chart = d3.select("#chart")
+    .attr("width", width)
+    .attr("height", height);
+
+
+// console.log(new Date())
+
+
+
+
+
+
+
+
+function fetchData(){
                 d3.json(realtimeURL, function(error,users){
                 console.log("users:",users);
                 d3.select('#users').html(users);
+
+
+
+
+
+
+
+
+
                 });
             }
+
+
+
+
+
 
             fetchData();
 
             setInterval(fetchData,frequency);
+
+
+
+
